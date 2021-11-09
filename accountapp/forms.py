418196f54ebf23d00b234e1bd4c23f3942f0ace2
raccoon2389 <35759061+raccoon2389@users.noreply.github.com>
2021-11-09 12:20:15 +0900

    Add files via upload

---
+++ accountapp/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class AccountUpdateForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].disabled = True
